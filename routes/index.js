74a90310dd213cb309b955fe660b8ea1ada37ccf
Brodybot <brodybotart@gmail.com>
2023-10-27 19:26:34 +0000

    erved static html home

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, 'home.html'));
 });
 
 module.exports = router;
